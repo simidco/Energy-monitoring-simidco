c324f0ebe0c792534ebcd5f50f4a5874f2f98475
simidco <eliaspoorkarim@gmail.com>
2026-09-08 12:18:54 +0330

    Add files via upload

--- app.py
+++ app.py
@@ -4968,7 +4968,8 @@ with tabs[20]:
     pdca_df = pd.DataFrame(pdca_summary)
     st.subheader("📊 جدول خلاصه PDCA")
     # قبل (خطا):
-    st.dataframe(pdca_df.style.applymap(lambda x: 'background-color: lightgreen' if x else 'background-color: lightcoral', subset=['Status']))
+    # استفاده از map به جای applymap (برای پانداس جدید)
+    st.dataframe(pdca_df.style.map(lambda x: 'background-color: lightgreen' if x else 'background-color: lightcoral', subset=['Status']))
 
        # بعد (اصلاح):
     st.dataframe(pdca_df.style.map(lambda x: 'background-color: lightgreen' if x else 'background-color: lightcoral', subset=['Status']))
